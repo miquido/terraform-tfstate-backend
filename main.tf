6cb8fec57d5e4162d09c2de6b36171693c05fde5
Konrad Obal <konrad.obal@miquido.com>
2019-06-04 17:07:13 +0200

    fix: Restrict all public access to s3 bucket

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 module "this" {
-  source              = "git@github.com:cloudposse/terraform-aws-tfstate-backend?ref=0.7.0"
-  namespace           = "${var.name}"
-  stage               = "${var.environment}"
-  name                = "terraform"
-  attributes          = ["state"]
-  region              = "${var.region}"
-  tags                = "${var.tags}"
-  block_public_acls   = "true"
-  block_public_policy = "true"
+  source                  = "git@github.com:cloudposse/terraform-aws-tfstate-backend?ref=0.7.0"
+  namespace               = "${var.name}"
+  stage                   = "${var.environment}"
+  name                    = "terraform"
+  attributes              = ["state"]
+  region                  = "${var.region}"
+  tags                    = "${var.tags}"
+  block_public_acls       = "true"
+  block_public_policy     = "true"
+  restrict_public_buckets = "true"
+  ignore_public_acls      = "true"
 }
