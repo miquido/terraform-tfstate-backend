250e52436cfe53c7cf99877ac60ce58e3baf0b91
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-09-27 08:28:58 +0200

    feat: CI

---
+++ examples/complete/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "task" {
+
+  source              = "../.."
+  cluster_arn         = "test"
+  container_cpu       = 0
+  container_image     = "test"
+  container_memory    = 0
+  container_tag       = "test"
+  environment         = "test"
+  name                = "test"
+  project             = "test"
+  region              = "test"
+  schedule_expression = "test"
+  security_groups     = []
+  subnets             = []
+}
