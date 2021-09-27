8e7167b965c32b4d2c8ea2e7e1130ff164d9d39c
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-09-27 08:48:08 +0200

    chore: remove schedule expression

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,16 +4,16 @@ provider "aws" {
 
 module "task" {
 
-  source              = "../.."
-  cluster_arn         = "test"
-  container_cpu       = 0
-  container_image     = "test"
-  container_memory    = 0
-  container_tag       = "test"
-  environment         = "test"
-  name                = "test"
-  project             = "test"
-  region              = "test"
-  security_groups     = []
-  subnets             = []
+  source           = "../.."
+  cluster_arn      = "test"
+  container_cpu    = 0
+  container_image  = "test"
+  container_memory = 0
+  container_tag    = "test"
+  environment      = "test"
+  name             = "test"
+  project          = "test"
+  region           = "test"
+  security_groups  = []
+  subnets          = []
 }
