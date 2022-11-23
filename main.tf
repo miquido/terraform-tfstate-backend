31dcbcd1b9d0b149c164d7c212f1ca02f6541d27
Krzysztof Kluska <krzysztof.kluska@miquido.com>
2022-11-23 13:51:55 +0100

    fix(tfstate-file): remove not needed executable flag from generated tfstate-backend.tf file

--- main.tf
+++ main.tf
@@ -47,7 +47,8 @@ module "this" {
 }
 
 resource "local_file" "terraform_backend_config" {
-  count    = var.terraform_backend_config_file_path != "" ? 1 : 0
-  content  = local.templatefile
-  filename = local.terraform_backend_config_file
+  count           = var.terraform_backend_config_file_path != "" ? 1 : 0
+  content         = local.templatefile
+  filename        = local.terraform_backend_config_file
+  file_permission = "0644"
 }
